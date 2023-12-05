1114f5af125aaa456349911a92e9272776d62c67
Vijay Singh <vijaysinghaashu@gmail.com>
2023-12-05 10:44:52 +0530

    base-code-created

---
+++ src/main/java/com/excellence_springboot_blog_post_management/SpringbootBlogPostManagementApplication.java
@@ -0,0 +1,13 @@
+package com.excellence_springboot_blog_post_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBlogPostManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBlogPostManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/excellence_springboot_blog_post_management/SpringbootBlogPostManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.excellence_springboot_blog_post_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBlogPostManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
